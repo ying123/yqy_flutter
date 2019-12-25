6fee9ad7bc019bcef0835ba664ee658a7820ce04
Rann <1227228155@qq.com>
2019-12-25 09:36:41 +0800

    登陆页面

---
+++ lib/ui/login/login_new_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:oktoast/oktoast.dart';
+import 'package:yqy_flutter/net/network_utils.dart';
+import 'package:yqy_flutter/route/r_router.dart';
+import 'dart:async';
+
+import 'package:yqy_flutter/route/routes.dart';
+import 'package:yqy_flutter/ui/login/bean/login_entity.dart';
+import 'package:yqy_flutter/utils/regex_utils.dart';
+import 'package:yqy_flutter/utils/user_utils.dart';
+
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+
+
+    );
+  }
+}
